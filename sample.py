e04ef4cff6a598e4b588616e4e0911f0dbe66ed8
sho0712 <hqmhn857@yahoo.co.jp>
2022-01-30 09:49:23 +0900

    first commit

---
+++ sample.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import chromedriver_binary
+
+
+def main():
+    food = 'とまと'
+    # ドライバーの立ち上げ（グーグルクロームを操作する人を立ち上げる）
+    driver = webdriver.Chorme()
+
+    # Googleにアクセス
+
+
+if __name__ == '__main__':
+    main()
