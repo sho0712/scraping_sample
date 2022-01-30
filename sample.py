a85fb28c8f655045cce60e40b442ac2569e605cf
sho0712 <hqmhn857@yahoo.co.jp>
2022-01-30 10:07:16 +0900

    gogle でお試し

--- sample.py
+++ sample.py
@@ -1,13 +1,20 @@
-from selenium import webdriver
+import time
+
+# noinspection PyUnresolvedReferences
 import chromedriver_binary
+from selenium import webdriver
 
 
 def main():
     food = 'とまと'
     # ドライバーの立ち上げ（グーグルクロームを操作する人を立ち上げる）
-    driver = webdriver.Chorme()
+    driver = webdriver.Chrome()
 
     # Googleにアクセス
+    driver.get('https://google.com')
+
+    time.sleep(10)
+    driver.close()
 
 
 if __name__ == '__main__':
